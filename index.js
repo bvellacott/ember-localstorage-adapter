d5260050f1062edaedccf18450ccf54c9c92876a
Benjamin Vellacott <bmvellacott@gmail.com>
2016-06-09 19:38:11 +0100

    minor edit to package.json before publishing

--- index.js
+++ index.js
@@ -2,9 +2,5 @@
 'use strict';
 
 module.exports = {
- 	name: 'ember-localstorage-adapter',
-	included: function(app) {
-       	// this._super.included(app);
-	   	app.import(app.bowerDirectory + '/where-clause-evaluate/dist/where-eval.js');
-	}
+ 	name: 'ember-localstorage-adapter'
 };
